5dbecd530e82053231fc259438bcd305bed2f708
Saikat Majumder <41925107+SAIKATMAJUMDER@users.noreply.github.com>
2019-02-17 13:25:27 +0600

    first time

---
+++ Find Doctor/Add_Doctor.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Find_Doctor
+{
+    /// <summary>
+    /// Interaction logic for Add_Doctor.xaml
+    /// </summary>
+    public partial class Add_Doctor : Window
+    {
+        public Add_Doctor()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
